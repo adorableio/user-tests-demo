78a159e228c80f4e7c881e0f2fdb1140faed13c8
Kevin Altman <kevin@itsthatguy.com>
2017-09-13 15:31:33 -0700

    logging for setup_apps

--- .buildkite/setup_apps.sh
+++ .buildkite/setup_apps.sh
@@ -9,12 +9,19 @@ function prepare_app() {
   local BRANCH_OR_SHA="master"
   pushd $1
     if [ "$1" = "$TRIGGERING_REPO" ]; then
+    echo "\n#################################################"
+    echo "repo-name: ${1}"
+    echo "triggered-repo: ${TRIGGERING_REPO}"
       [[ -z "$TRIGGERING_SHA" ]] && BRANCH_OR_SHA=$TRIGGERING_SHA
+      echo "branch-or-sha: ${BRANCH_OR_SHA}"
     fi
     git checkout .
+    echo "\nchecking out ${BRANCH_OR_SHA}"
     git checkout $BRANCH_OR_SHA
+    echo "pulling latest"
     git pull
     npm install
+    echo "#################################################\n"
   popd
 }
 
