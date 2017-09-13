64d809b4d748d761619d39c92553ff12f4cdb141
Ryland Herrick <ryalnd@gmail.com>
2017-09-13 14:36:58 -0700

    Kill servers in a pre-exit hook
    So that the build doesn't always fail!

--- .buildkite/run_cypress_tests.sh
+++ .buildkite/run_cypress_tests.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-source ".buildkite/shared_functions"
-
 function wait_for_port() {
   local port="$1"
   while lsof -i tcp:"${port}" > /dev/null; do
@@ -10,18 +8,12 @@ function wait_for_port() {
   done
 }
 
-# run the server in the background
+echo "starting servers in background..."
 npm start &
-NPM_SERVER_PID="$!"
 
 echo "waiting for servers to start..."
 wait_for_port 3000
 wait_for_port 3002
 
-echo "servers started, running tests"
+echo "servers started, running tests..."
 npm run test
-
-echo "tests finished, stopping servers"
-kill_port 3000
-kill_port 3002
-kill "${NPM_SERVER_PID}"
