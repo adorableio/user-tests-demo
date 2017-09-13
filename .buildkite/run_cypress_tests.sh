db312737b7c229662306fe2be3456ade464b2e8e
Ryland Herrick <ryalnd@gmail.com>
2017-09-13 13:40:20 -0700

    Run server and wait for it to bind before testing

---
+++ .buildkite/run_cypress_tests.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# run the server in the background
+npm start &
+NPM_SERVER_PID="$!"
+
+# wait for port 3000 to be bound
+while ! nc localhost 3000; do
+  sleep 1
+done
+
+echo "server started"
+$(npm bin)/cypress run
