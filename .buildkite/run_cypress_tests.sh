7a4007710455692b80dc581dd50256e8e69d2e5c
Ryland Herrick <ryalnd@gmail.com>
2017-09-13 13:55:37 -0700

    debugging

--- .buildkite/run_cypress_tests.sh
+++ .buildkite/run_cypress_tests.sh
@@ -6,11 +6,13 @@ NPM_SERVER_PID="$!"
 
 # wait for port 3000 to be bound
 while ! nc localhost 3000; do
+  echo "waiting for 3000"
   sleep 1
 done
 
 # wait for port 3002 to be bound
 while ! nc localhost 3002; do
+  echo "waiting for 3002"
   sleep 1
 done
 
